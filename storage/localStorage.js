2f467032e43676d8fe9e30abb769039d8bd7d3ec
Fatima Rakiep <rakiepfatima@gmail.com>
2025-09-21 12:10:07 +0200

    Create storage folder and added localStorage.js to handle task storge and the retrieval from localStorage

---
+++ storage/localStorage.js
@@ -0,0 +1,28 @@
+import { initialTasks } from "../../initialData.js";
+
+/**
+ * Loads tasks from localStorage or initializes with initialTaskstasks.
+ * @returns {Array<Object>} The array of tasks.
+ */
+export function loadTasksFromStorage() {
+    const stored = localStorage.getItem("tasks");
+    if (stored) {
+        try {
+            return JSON.parse(stored);
+        } catch (error) {
+            console.error("Error parsing tasks from localStorage:", error);
+        }
+    }
+
+ // If no tasks in storage, initialize with initialTasks
+localStorage.setItem("tasks", JSON.stringify(initialTasks));
+return initialTasks;
+}
+
+/**
+ * Saves the given task array to localStorage.
+ * @param {Array<Object>} tasks - The array of tasks to save.
+ */
+export function saveTasksToStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
